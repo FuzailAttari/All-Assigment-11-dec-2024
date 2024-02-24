dec6eb44c2a49a19111ed200458ab5edc9e47d48
Fuzail Raza Attari (Manai) <hunailattari@gmail.com>
2024-02-24 13:02:50 +0500

    Add Java Scrip Folder

---
+++ Java Scrip/App.js
@@ -0,0 +1,41 @@
+// ==================Heading==========================
+
+document.write
+( "<center>"+
+"<font color=yellow>"+
+"<table border=3px bordercolor=red  cellpadding=20px cellspacing=10px bgcolor=black> "+
+"<tr>"+ 
+"<th>"+ "<h1>"+"Java Scrip Class Start 23-Feb-2024 Practic Work"+"</th>"+"</table>"+"<br>")
+
+// ========================Typesof=================================
+
+document.write("<font color=black>"+ "<h2>" + typeof("Name")+("<br>") )
+document.write(typeof(12)+("<br>"))
+document.write(typeof(true)+("<br>"))
+document.write(typeof(12.4444)+("<br>"))
+
+// ===============================Table===============================
+
+document.write(
+    "<center>"+
+    "<font color=yellow>"+
+    "<table border=3px bordercolor=red  cellpadding=20px cellspacing=10px bgcolor=black> "+
+    "<tr>"+
+    "<th>"+ "First Name"+"</th>"+
+    "<th>"+ "Last Name"+"</th>"+
+    "<th>"+ "Contact"+"</th>"+
+    "<th>"+ "Email"+"</th>"
+     +"/<tr>"
+
+    + "<tr>"+
+    "<td>"+ "Fuzail"+"</td>"+
+    "<td>"+ "Manai"+"</td>"+
+    "<td>"+ "0321-2755408"+"</td>"+
+    "<td>"+ "Fuzailali49@yahoo.com"+"</td>"
+     +"/<tr>"
+
+    +"</table>"
+    +"</font>"
+    +"</center"
+)
+
