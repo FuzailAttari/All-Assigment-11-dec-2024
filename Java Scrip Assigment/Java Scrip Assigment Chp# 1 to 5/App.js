1a71e5e75ea4009952846d85144e7c0c5a17226d
Fuzail Raza Attari (Manai) <hunailattari@gmail.com>
2024-02-28 19:02:06 +0500

    Java Scrip Assignment  Chp 1 to 5

---
+++ Java Scrip Assigment/Java Scrip Assigment Chp# 1 to 5/App.js
@@ -0,0 +1,130 @@
+// ==========Chapter01 ALERT ============
+ 
+     //Q-1    // alert()
+ 
+     //Q-2    // alert("Hello World" )
+ 
+     //Q-3    // alert("Hello World \nMy Name is Fuzail" )
+
+     //Q-4    // alert("Hello World " )      
+           // alert("My Name is Fuzail" )      
+          // alert("I am Learing JavaScrip" )      
+
+     //Q-5   // document.write("Hello world!");
+
+    
+     //Q-7  // <body>  <script src="app.js"></script> </body>;
+   
+        // ==========Chapter02 Variable ============
+
+     // Q1  var Username ;
+
+     // Q2   // var myName = "Ahmedraza" 
+            // alert(myName)
+
+     // Q3   // var Message ;
+            //  Message = "Hello World"; 
+            //  Alert(Message);
+
+     // Q4   // var Studentname = "Ali Khan" ;
+            // var Studentage = 10 ;
+            // var Studentaddress = "lalokheth" ;
+            // var StudentGender = "Male" ;
+
+            //  alert (" Studentname is " + Studentname ) 
+            //  alert ("Studentage is "+ Studentage) ;
+            //  alert ("Studentaddress is " + Studentaddress) ;
+            //  alert ("StudentGender is " + StudentGender ) ;
+
+     // Q5   // var wannaEat= "Pizza\nPizz\nPiz\nPi\nP"
+            // alert(wannaEat)
+            
+            
+     // Q7   // var msg="My email address is :"
+            // var email="hunailattari@gmail.com"
+            // alert(msg +" "+ email) 
+
+     // Q8  // var msgBook="I am trying to learn from the book : "
+           // var book="A smarter way to learn JavaScript."
+           // alert(msgBook+" "+book)
+
+     //Q9  //    var fullname = "My Name is Fuzail Manai";
+           //    var Age = "I am 32 years old";
+           //    var study = "I study in W M A"
+           //    var Talk = "I take Mobile & Cloud Computing";
+           //    var br = "<br>"
+
+           //    document.write( "<h2 " + br + fullname + br + Age + br + study + br +  Talk  );
+
+    //Q10  // document.write("Yah! I can write HTML content through JavaScript")
+
+        // ==========Chapter03 Variable ============
+
+
+            
+    //Q1   // var age = 32;
+ 
+          // alert("i am " + age + " years old");
+
+    // Q2    var Vc= 21;
+          // alert ( "You are vist "+" "+ Vc + " " + "Time this Side")
+
+    //Q3  //  var By= 1991;
+          //  document.write( "My Birthday Year is" + " " + By + "<br>")
+          //  document.write( "Data type of my declared variable is number")
+
+    //Q4       var Visitorname= "Fuzail Manai";
+          //   var Producttitle= "T-shirt"; 
+          //   var Quantity= 5;
+          //   var store ="on XYZ Clothing store"
+          //    document.write( "<b>" + Visitorname +" "+ "Ordered" +" "+Quantity+ " "+ Producttitle +" "+ store) 
+
+         // ==========Chapter04 VariableVARIABLE NAMES: LEGAL & ILLEGAL ============
+
+    //Q2   var firstName= "Fuzail";
+          // var sceoundName= "Manai";
+          // var age= "32";
+          // var Marge= firstName + sceoundName + age ;
+          // document.write(Marge) ;
+
+    //Q2      // Five illegal variables
+
+           // var illegal_1 = "A number cannot be the initial character";
+           // var illegal_2 = "A variable name cannot start with a numeral";
+           // var illegal_3 = "Spaces are not allowed in variable names";
+           // var illegal_4 = "Hyphens are not allowed in variable names";
+           // var illegal_5 = "The variable name can not be the same as JavaScript keywords";
+
+            // Five legal variables
+        
+            // var legal_1="A letter"
+            // var legal_2="dollar sign( $ )"
+            // var legal_3="underscore ( _ )"
+            // var legal_4="letter then Number 1"
+            // var legal_5=""
+        
+    //Q3    // var a = "<h3>Rules for naming JS variables</h3>";
+            // var b = "Variable names can only contain" + "numbers," +"$ and_ " + "For example $my_1stVariable";
+            // var c = "Variables must begin with a" + "$name," + "_name" + "or name";
+            // var d = "Variable names are case" + " sensitive";
+            // var e = "Variable names should not be JS" + " Keywards";
+
+            // document.write(a + "<br/>" + b + "<br/>" + c + "<br/>" + d + "<br/>" + e);
+
+
+         // ==========Chapter 05 MATH EXPRESSIONSL ============
+
+    //Q1   //   var num1= 13+9                   
+           //   alert("Sum of 13 from 9 is "+" " + num1 )
+
+           //   var num1= 13-9                   
+           //   alert("Result of suntracting 9 from 13 is"+" " + num1 )
+
+           //   var num1= 13*9                   
+           //   alert("Result of multiplying 13 from 9 is"+" " + num1 )  
+
+           //   var num1= 13/9                   
+           //   alert("Result of Dividing 13 from 9 is"+" " + num1 )
+
+            //  var num1= 13%9                   
+            //  alert("Remainder of Dividing 9 from 13 is"+" " + num1 )
