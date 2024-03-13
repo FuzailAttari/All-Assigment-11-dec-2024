3ddb795bf18ace239ef611cc5d37c0941f87de23
Fuzail Raza Attari (Manai) <hunailattari@gmail.com>
2024-03-13 14:53:23 +0500

    TABLE SHEET

--- Java Scrip Assigment/Java Scrip Task File/App.js
+++ Java Scrip Assigment/Java Scrip Task File/App.js
@@ -39,3 +39,26 @@ document.write(
     +"</center"
 )
 
+// ============loop============================
+    document.write(` 
+    
+    <center> <h1>  <font color=yellow>   <table  border "2px" bordercolor=red 
+    cellpadding= "10px" cellspacing= 5px bgcolor=black> <td> Table </td> </table>` )
+
+var userInput = prompt ("Enter Number");
+var userInput1 = prompt ("Enter Number");
+var userRange = prompt("Enter Table range");
+
+
+for ( i = 1;  i<=userRange; i++){
+   
+    document.write( `
+    <center> <font color=yellow> 
+    <table border "2px" bordercolor=red width = "30%"   cellpadding= "10px" cellspacing= 5px bgcolor=black > 
+    <tr>
+    <td >${userInput +" "+ "X" + " " + i + " " + " = " + userInput*i+ "<br>"} </td>
+
+    <td>${userInput1 +" "+ "X" + " " + i + " " + " = " + userInput1*i + "<br>"} </td>
+    </tr> 
+    </table> ` )
+}
