cf47a505843003b00895c0cbe6f212c440547dc1
Fuzail Raza Attari (Manai) <hunailattari@gmail.com>
2024-03-24 17:20:27 +0500

    changing

--- Java Scrip Assigment/Java Scrip Assigment Chp# 1 to 5/App.js
+++ Java Scrip Assigment/Java Scrip Assigment Chp# 1 to 5/App.js
@@ -128,3 +128,139 @@
 
             //  var num1= 13%9                   
             //  alert("Remainder of Dividing 9 from 13 is"+" " + num1 )
+
+      //  Q2
+                  
+            // 3. Do the following using JS Mathematic Expressions
+            // a. Declare a variable.
+            // b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
+            // c. Initialize the variable with some number.
+            // d. Show the value of variable in your browser like “Initial value: 5”.
+            // e. Increment the variable.
+            // f. Show the value of variable in your browser like “Value after increment is: 6”.
+            // g. Add 7 to the variable.
+            // h. Show the value of variable in your browser like “Value after addition is: 13”.
+            // i. Decrement the variable.
+            // j. Show the value of variable in your browser like “Value after decrement is: 12”.
+            // k. Show the remainder after dividing the variable’s value by 3.
+            // l. Output : “The remainder is : 0”.         
+
+            // var num;
+
+            // document.write("Value after variable declaration is: " + num)
+            
+            // num = 5;
+            
+            // document.write("<br/> Initial value: " + num)
+            
+            // num = num + 1
+            
+            // document.write("<br/> Value after increment is: " + num)
+            
+            // num = num + 7
+            
+            // document.write("<br/> Value after addition is: " + num)
+            
+            // num = num - 1
+            
+            // document.write("<br/> Value after decrement is: " + num)
+            
+            // num = num % 3
+            
+            // document.write("<br/> The remainder is: " + num)
+
+            // ================Q-4========================
+
+            // var num = 5
+            // var total = 5*600      
+            // document.write( ` Total Cost to buy ${num} tickets to a movie is ${total} PKR`)
+
+            // ================Q-5========================
+
+            // var num = 5 ;     
+            // var br = "<br>"
+            // document.write(`${num} X 1 = ${num*1} ${br}  `)
+            // document.write(`${num} X 2 = ${num*2} ${br} `)
+            // document.write(`${num} X 2 = ${num*3} ${br} `)
+            // document.write(`${num} X 2 = ${num*4} ${br} `)
+            // document.write(`${num} X 2 = ${num*5} ${br} `)
+            // document.write(`${num} X 2 = ${num*6} ${br} `)
+            // document.write(`${num} X 2 = ${num*7} ${br} `)
+            // document.write(`${num} X 2 = ${num*8} ${br} `)
+            // document.write(`${num} X 2 = ${num*9} ${br} `)
+            // document.write(`${num} X 2 = ${num*10} ${br} `)
+
+      //       var num = 4
+      //   for(i=1; i<=10; i++  ){
+      //       document.write(`${num} X ${i} = ${num*i} <br> `)
+      //   }    
+
+            // ================Q-6========================
+
+      //       var fahrenheit = 70;
+
+      //       var celsius = 25;
+
+      // document.write(celsius, "<sup>o</sup>C is ", (celsius * 9 / 5) + 32, "<sup>o</sup>F <br/>")
+      // document.write(fahrenheit, "<sup>o</sup>F is ", (fahrenheit - 32) * 5 / 9, "<sup>o</sup>C")
+
+            // ================Q-7========================
+
+            // document.write(`<h2> Shopping Cart </h2> <br>  `)
+
+            // var item1= 560;
+            // var item2= 200;
+            // var Qty1= 2;
+            // var Qty2= 5;
+            // var Sc= 150;
+            // var total = (item1 * Qty1) + ( item2 * Qty2) + Sc 
+
+            // document.write(`Pirce Of Item 1 is ${item1} <br>`)
+            // document.write(`Quantity Of Item 1 is ${Qty1} <br>`)
+            // document.write(`Pirce Of Item 2 is ${item2} <br>`)
+            // document.write(`Quantity Of Item 2 is ${Qty2} <br> <br>`)
+            // document.write(`Shipping Charges ${Sc} <br> <br>`)
+
+            // document.write(`Total Cost Of Your Order is ${total} PKR `)
+
+            // ================Q-8========================
+
+            // document.write(`<h2> Marks Sheet </h2> <br>  `)
+
+            // var Tm = 1200 ; 
+            // var Mo = 900
+
+
+            // var toatl = ( Mo / Tm ) * 100;
+
+            // document.write( ` <h2> Total Marks :  ${Tm} <br> `)
+            // document.write( ` Marks obtained : ${Mo } <br> `)
+            // document.write( ` Percentage :  ${toatl } % <br> `)
+
+            // ================Q-9========================
+            
+      // var Usdollar = 10;
+      // var Saudiriyals = 25;
+      // var convertUs =  104;
+      // var convertSr =  28;
+      // var Total =( Usdollar * convertUs) + ( Saudiriyals * convertSr) ;
+
+      // document.write( `<h1> Currency in PKR </h1> <br> <br> `)
+      // document.write( ` Total Currency in PKR : ${Total}`)
+
+            // ================Q-10========================
+
+            // var num = 5;
+            // var num1 = 10
+            // var num2 = 2;
+            // var Total = ((num * num1)) / num2 ;
+            // alert( Total)
+       
+            // ================Q-10========================
+
+            
+
+
+
+
+
