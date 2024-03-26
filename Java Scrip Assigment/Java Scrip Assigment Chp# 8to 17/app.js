8b8ab403c2077052fbe40587ee31f7705648b5cd
Fuzail Raza Attari (Manai) <hunailattari@gmail.com>
2024-03-26 20:00:05 +0500

    Q6+Q7+Q8 Solve

--- Java Scrip Assigment/Java Scrip Assigment Chp# 8to 17/app.js
+++ Java Scrip Assigment/Java Scrip Assigment Chp# 8to 17/app.js
@@ -4,9 +4,9 @@
     // Write a program that takes input a name from user & greet the
     // user like this:
 
-    var userNAme= prompt( "Enter Your Name");
-    alert(userNAme)
-    alert("Hi Fuzail")
+    // var userNAme= prompt( "Enter Your Name");
+    // alert(userNAme)
+    // alert("Hi Fuzail")
 
    // ========================================================
 
@@ -16,21 +16,21 @@
      // a new number, multiplication table of 5 should be displayed by
      // default.    
     
-        var number = +prompt("Enter a number:", 5);
+        // var number = +prompt("Enter a number:", 5);
     
-        document.write("<h2>Multiplication Table of ", number, "</h2>");
+        // document.write("<h2>Multiplication Table of ", number, "</h2>");
     
-        document.write(number, " x 1 = ", number * 1, "<br>");
-        document.write(number, " x 2 = ", number * 2, "<br>");
-        document.write(number, " x 3 = ", number * 3, "<br>");
-        document.write(number, " x 4 = ", number * 4, "<br>");
-        document.write(number, " x 5 = ", number * 5, "<br>");
-        document.write(number, " x 6 = ", number * 6, "<br>");
-        document.write(number, " x 7 = ", number * 7, "<br>");
-        document.write(number, " x 8 = ", number * 8, "<br>");
-        document.write(number, " x 9 = ", number * 9, "<br>");
+        // document.write(number, " x 1 = ", number * 1, "<br>");
+        // document.write(number, " x 2 = ", number * 2, "<br>");
+        // document.write(number, " x 3 = ", number * 3, "<br>");
+        // document.write(number, " x 4 = ", number * 4, "<br>");
+        // document.write(number, " x 5 = ", number * 5, "<br>");
+        // document.write(number, " x 6 = ", number * 6, "<br>");
+        // document.write(number, " x 7 = ", number * 7, "<br>");
+        // document.write(number, " x 8 = ", number * 8, "<br>");
+        // document.write(number, " x 9 = ", number * 9, "<br>");
 
-        document.write(number, " x 10 = ", number * 10);
+        // document.write(number, " x 10 = ", number * 10);
     
     // ========================================================
 
@@ -39,10 +39,10 @@
         // enters “Karachi”, welcome the user like this: “Welcome to city
         // of lights”
 
-        var Cityname = prompt("Enter Your City ")
-        if ( Cityname === "karachi" ){
-              alert( ` Welcome to city of light`)
-            }
+        // var Cityname = prompt("Enter Your City ")
+        // if ( Cityname === "karachi" ){
+        //       alert( ` Welcome to city of light`)
+            // }
 
     // ========================================================
 
@@ -52,61 +52,133 @@
         // user is male, give the message: Good Morning Sir. If the user is
         // female, give the message: Good Morning Ma’am.
 
-        var Gender = prompt( "Enter Gender");
+        // var Gender = prompt( "Enter Gender");
 
-        if ( Gender === "male"){
-            alert(`Good Morning Sir`)
+        // if ( Gender === "male"){
+        //     alert(`Good Morning Sir`)
 
-        }
+        // }
         
-        if( Gender=== "female"  ){
-            alert(`Good Morning Ma'am`)
-        }
-        else{
-            alert(`Kon hai tw`)
-        }
+        // if( Gender=== "female"  ){
+        //     alert(`Good Morning Ma'am`)
+        // }
+        // else{
+        //     alert(`Kon hai tw`)
+        // }
 
     // ========================================================
 
         // Q5. Write a program to take input color of road traffic signal from
         // the user & show the message according to this table:
 
-        var Color = prompt( "Enter Color");
-        var Color1 = prompt( "Enter Color");
-        var Color2 = prompt( "Enter Color");
+        // var Color = prompt( "Enter Color");
+        // var Color1 = prompt( "Enter Color");
+        // var Color2 = prompt( "Enter Color");
         
-        if( Color === "red") {
-            if( Color1 === "yellow"){
-                if( Color2 === "green"){
+        // if( Color === "red") {
+        //     if( Color1 === "yellow"){
+        //         if( Color2 === "green"){
                     
                     
-                    document.write(`
-                    <table Center border "1px" width="50%"  > 
-                    <tr> <th> Signal Color </th>
-                    <th> Message </th> 
-                    </tr> 
-                    <tr>
-                    <td> ${Color} </td>
-                    <td> Vehicles Must Stop  </td>
-                    </tr>
-                    <tr>
-                    <td> ${Color1} </td>
-                    <td> Vehicles Should Get Ready To Move </td>  
-                    </tr>
-                    <tr>
-                    <td> ${Color2} </td>
-                    <td> Vehicles can Move now </td>  
-                    </tr>
-                    </table>`)
-        }     
+        //             document.write(`
+        //             <table Center border "1px" width="50%"  > 
+        //             <tr> <th> Signal Color </th>
+        //             <th> Message </th> 
+        //             </tr> 
+        //             <tr>
+        //             <td > ${Color} </td>
+        //             <td> Vehicles Must Stop  </td>
+        //             </tr>
+        //             <tr>
+        //             <td> ${Color1} </td>
+        //             <td> Vehicles Should Get Ready To Move </td>  
+        //             </tr>
+        //             <tr>
+        //             <td> ${Color2} </td>
+        //             <td> Vehicles can Move now </td>  
+        //             </tr>
+        //             </table>`)
+        // }     
             
-        }
+        // }
 
-        }
+        // }
         
     // ========================================================
 
-            
+    //    Q6 . Write a program to take input max age & current age from
+    //         user. If the current age is less than or equal to max age, show
+    //         the message “You are welcome”
 
+    //         var age = prompt("Enter Your Age");
+    //         if(age==="25"){
+    //         alert("Welcome to page")
+    //         }
+    //         else{
+    //         alert( "abi bacho ho" )
+    //         }
+
+    // ========================================================
     
+    // Q7. Write a program to take input remaining fuel in car (in litres)
+    // from user. If the current fuel is less than 0.25litres, show the
+    // message “Please refill the fuel in your car”
+    
+
+    //  var remainingFuel = +prompt("Enter the remaining fuel in your car (in litres):");
+
+    //     if (remainingFuel < 0.25) {
+    //     alert("Please refill the fuel in your car");
+    //     }
+
+    // ========================================================
+    
+    // Q8 Run this script, & check whether alert message would be displayed or not.Record the outputs.
+
+        // var a = 4;
+        // if (++a === 5) {
+        //     alert("given condition for variable a is true");
+        // }
+
+        // var b = 82;
+        // if (++b === 83) {
+        //     alert("given condition for variab b is true");  
+        // }
+        // var c = 12;
+        // if (++c === 13) {
+        //     alert("condition 1 is true");     
+        // }
+        // if (c === 13) {
+        //     alert("condition 2 is true");
+        // }
+        // if (c++ < 14) {
+        //     alert("condition 3 is true"); 
+        // }
+        // if (c === 14) {
+        //     alert("condition 4 is true");
+        // }
+        // var materialCost = 20000;
+        // var laborCost = 2000;
+        // var totalCost = materialCost + laborCost;
+
+        // if (totalCost === laborCost + materialCost) {
+        //     alert("The cost equals");
+        // }
+        // if (true) {
+        //     alert("True");
+        // }
+        // if (false) {
+        //     alert("False"); //issue thhis
+        // }
+        // if ("car" < "cat") {
+        //     alert("car is smaller than cat");
+        // }
         
+    // ========================================================
+
+    // Q9
+    // Write a program to take input the marks obtained in three
+    // subjects & total marks. Compute & show the resulting
+    // percentage on your page. Take percentage & compute grade as
+    // per following table: 
+
